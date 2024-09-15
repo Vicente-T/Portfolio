8fa9a5fbbc20c0974bd3eb48e8a2700eae7049f6
Vicente Teixeira <vicenteteixeira05@gmail.com>
2024-09-15 17:10:02 +0100

    feat

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,31 +1,29 @@
-import React from "react";
+import React from 'react';
 
-import './footer.css';
+import './footer-styles.css';
 
+const Footer = () => {
+	return (
+		<footer className='footer'>
+			<div className='footer__container container'>
+				<h1 className='footer__title'>Vicente</h1>
 
-const Footer = () => { 
-    return (
-			<footer className='footer'>
-				<div className='footer__container container'>
-					<h1 className='footer__title'>Vicente</h1>
+				<ul className='footer__list'>
+					<li>
+						<a href='#about' className='footer__link'>
+							About
+						</a>
+					</li>
+					<li>
+						<a href='#skills' className='footer__link'>
+							Skills
+						</a>
+					</li>
+				</ul>
+				<span className='footer__copy'>© 2022. All rights reserved</span>
+			</div>
+		</footer>
+	);
+};
 
-					<ul className='footer__list'>
-						<li>
-							<a href='#about' className='footer__link'>
-								About
-							</a>
-						</li>
-						<li>
-							<a href='#skills' className='footer__link'>
-								Skills
-							</a>
-						</li>
-					</ul>
-                    <span className="footer__copy">© 2022. All rights reserved</span>
-					
-				</div>
-			</footer>
-		);
-}
-
-export default Footer
+export default Footer;
